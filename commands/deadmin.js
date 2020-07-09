b18082cd1f0e885479cd21a99f8c8764f10b2a21
Thatcooldevguy <tsommers150@gmail.com>
2020-07-09 17:38:42 -0400

    1.0.1.3.1

--- commands/deadmin.js
+++ commands/deadmin.js
@@ -1,6 +1,16 @@
 const Discord = require("discord.js");
+const PermsDeined = new Discord.MessageEmbed()
+    .setTitle("You can't do that!")
+    .setColor(0x00AE86)
+    .setDescription("Either your not a Admin, or you dont have ``ADMINISTRATOR`` permission on your role.")
+    .setTimestamp()
 let role_target =  message.guild.roles.cache.find(r => r.name === "Admins");
 exports.run = (client, message, args) => {
+    if(!role_target){
+        return message.channel.send("Could not add the role to user. -192")
+    }
+    if (!message.member.roles.cache.has(role_target.id))
+        return message.reply(PermsDeined);
     let member = message.mentions.members.first();
     member.removeRole(role_target)
     message.channel.send("Removed admin for:" + args)
--- commands/mod.js
+++ commands/mod.js
@@ -1,6 +1,12 @@
 const Discord = require("discord.js");
 let role_target =  message.guild.roles.cache.find(r => r.name === "Mods");
+let role_target2 =  message.guild.roles.cache.find(r => r.name === "Admins");
 exports.run = (client, message, args) => {
+    if(!role_target2) {
+        return message.channel.send("Could not add the role to user. -192")
+    }
+    if (!message.member.roles.cache.has(role_target2.id))
+         return message.reply(PermsDeined);
     let member = message.mentions.members.first();
     member.addRole(role_target)
     message.channel.send("Modded:" + args)
