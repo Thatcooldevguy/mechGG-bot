27639d1ab6e1045868e0163cd1bb2c4fc7e578e2
Thatcooldevguy <tsommers150@gmail.com>
2020-07-09 22:00:26 -0400

    1.2.0

---
+++ commands/connect.js
@@ -0,0 +1,7 @@
+exports.run = async (client, message, args) => {
+    if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+}
