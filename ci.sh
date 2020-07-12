a109267029f408c75520366d9eb286e214d1a2e7
Thatcooldevguy <tsommers150@gmail.com>
2020-07-11 20:46:20 -0400

    add deploy (auto)

--- ci.sh
+++ ci.sh
@@ -5,7 +5,9 @@ jshint commands/
 jshint events/
 jshint tests/
 jshint index.js
-echo "[OKAY] jshint threw no errors! Starting test 2/2: Json Check"
+echo "[OKAY] jshint threw no errors! Starting test 2/3: Json Check"
 node tests/TestJson.js
-echo "[OKAY] Json test done. Marking as exit code 0.. "
+echo "[OKAY] Json threw no errors! Starting test 3/3: Deploy Script Check"
+bash deployment_tool/deploy.sh
+echo "[OKAY] all tests done. Marking as exit code 0.. "
 exit 0
