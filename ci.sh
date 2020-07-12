17679c8a744c0712f1fcb85998b21860a663aeb3
Thatcooldevguy <tsommers150@gmail.com>
2020-07-11 20:51:13 -0400

    debug 2

--- ci.sh
+++ ci.sh
@@ -9,6 +9,7 @@ echo "[OKAY] jshint threw no errors! Starting test 2/3: Json Check"
 node tests/TestJson.js
 echo "[OKAY] Json threw no errors! Starting test 3/3: Deploy Script Check"
 pwd
+ls
 bash deployment_tool/deploy.sh
 echo "[OKAY] all tests done. Marking as exit code 0.. "
 exit 0
